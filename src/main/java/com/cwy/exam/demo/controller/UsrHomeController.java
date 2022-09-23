7ffd0a796f21f5f30d86ec60c75a00e7d7fbb327
woooyoung <axdsw121@gmail.com>
2022-09-23 17:44:03 +0900

    count 변수의 값 초기화

--- src/main/java/com/cwy/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/cwy/exam/demo/controller/UsrHomeController.java
@@ -6,21 +6,33 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
+	private int count;
+
+	public UsrHomeController() {
+		count = 0;
+	}
+
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String showMain() {
 		return "안녕하세요";
 	}
-	
+
 	@RequestMapping("/usr/home/main2")
 	@ResponseBody
 	public String showMain2() {
 		return "반갑습니다";
 	}
-	
+
 	@RequestMapping("/usr/home/main3")
 	@ResponseBody
 	public String showMain3() {
 		return "잘가요";
+	}
+
+	@RequestMapping("/usr/home/main4")
+	@ResponseBody
+	public int showMain4() { 
+		return count++;
 	}
 }
