d493dd688bff8c88c1c9bf0dfc25018072bb987f
woooyoung <axdsw121@gmail.com>
2022-09-23 16:36:30 +0900

    환경 세팅

---
+++ src/main/java/com/cwy/exam/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.cwy.exam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
