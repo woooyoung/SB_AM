044d04baff02e834dd25efb42ed9c4261311d93d
woooyoung <axdsw121@gmail.com>
2022-09-26 16:48:03 +0900

    게시물 테스트데이터 10개 생성

--- src/main/java/com/cwy/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/cwy/exam/demo/controller/UsrArticleController.java
@@ -18,6 +18,21 @@ public class UsrArticleController {
 	public UsrArticleController() {
 		lastArticleId = 0;
 		articles = new ArrayList<>();
+
+		makeTestData();
+	}
+
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			int id = lastArticleId + 1;
+			String title = "제목 " + i;
+			String body = "내용 " + i;
+
+			Article article = new Article(id, title, body);
+
+			articles.add(article);
+			lastArticleId = id;
+		}
 	}
 
 	@RequestMapping("/usr/article/doAdd")
@@ -35,6 +50,6 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
-		return articles ;
+		return articles;
 	}
 }
