05b1e8a494eecb1e8ec4559dfb75f012a628956c
woooyoung <axdsw121@gmail.com>
2022-09-26 18:15:19 +0900

    Object Class, 게시물 상세보기 기능

--- src/main/java/com/cwy/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/cwy/exam/demo/controller/UsrArticleController.java
@@ -97,7 +97,7 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
-	public String doModify(int id, String title, String body) {
+	public Object doModify(int id, String title, String body) {
 		Article article = getArticle(id);
 
 		if (article == null) {
@@ -106,7 +106,19 @@ public class UsrArticleController {
 
 		modifyArticle(id, title, body);
 
-		return id + "번 게시물을 수정했습니다";
+		return article;
+	}
+
+	@RequestMapping("/usr/article/getArticle")
+	@ResponseBody
+	public Object getArticleAction(int id) {
+		Article article = getArticle(id);
+
+		if (article == null) {
+			return id + "번 게시물은 존재하지 않습니다";
+		}
+
+		return article;
 	}
 
 }
--- src/main/java/com/cwy/exam/demo/vo/Article.java
+++ src/main/java/com/cwy/exam/demo/vo/Article.java
@@ -11,5 +11,4 @@ public class Article {
 	private int id;
 	private String title;
 	private String body;
-
 }
