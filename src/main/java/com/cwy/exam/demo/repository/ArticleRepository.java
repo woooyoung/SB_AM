7797ad7179b0cf2c6d2e9bbf84eadb0c47edfc59
woooyoung <axdsw121@gmail.com>
2022-09-27 18:47:19 +0900

    INSERT 쿼리 실행되도록 return 타입 수정, last_insert_id()

--- src/main/java/com/cwy/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/cwy/exam/demo/repository/ArticleRepository.java
@@ -3,6 +3,7 @@ package com.cwy.exam.demo.repository;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
@@ -14,7 +15,8 @@ public interface ArticleRepository {
 
 	// INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = ?,
 	// `body` = ?;
-	public Article writeArticle(String title, String body);
+	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title},`body` = #{body}")
+	public void writeArticle(String title, String body);
 
 	// SELECT * FROM article WHERE id = ?;
 	@Select("SELECT * FROM article WHERE id = #{id}")
@@ -31,5 +33,8 @@ public interface ArticleRepository {
 	// UPDATE article SET title = ?, `body` = ?, updateDate = NOW() WHERE id = ?;
 	@Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
 	public void modifyArticle(int id, String title, String body);
+
+	@Select("SELECT LAST_INSERT_ID()")
+	public int getLastInsertId();
 
 }
