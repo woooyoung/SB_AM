7a5bd9f3692eda49666d24310a86aaa5676613d9
woooyoung <axdsw121@gmail.com>
2022-09-28 17:45:20 +0900

    회원가입 준비, UsrMemberController,MemberService 추가

---
+++ src/main/java/com/cwy/exam/demo/service/MemberService.java
@@ -0,0 +1,13 @@
+package com.cwy.exam.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+//	private MemberRepository memberRepository;
+
+	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
+//		memberRepository.join(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email);
+
+	}
+}
