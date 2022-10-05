0279b9a05bd433bc76bb16f1bc4f0f8b45f7c2f0
woooyoung <axdsw121@gmail.com>
2022-10-05 17:59:25 +0900

    JSP 연결

--- src/main/java/com/cwy/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/cwy/exam/demo/controller/UsrHomeController.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
-	@RequestMapping("usr/home/main")
-	@ResponseBody
-	public String getString() {
-		return "hi";
+	@RequestMapping("/usr/home/main")
+	public String showMain() {
+		return "usr/home/main";
+	}
+
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:/usr/home/main";
 	}
 }
