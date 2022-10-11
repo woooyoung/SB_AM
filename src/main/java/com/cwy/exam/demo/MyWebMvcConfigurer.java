63cd2358ae2b5012362cef4d623ee692ebf19eed
woooyoung <axdsw121@gmail.com>
2022-10-11 18:15:50 +0900

    BeforeActionInterceptor registry에 등록

---
+++ src/main/java/com/cwy/exam/demo/MyWebMvcConfigurer.java
@@ -0,0 +1,23 @@
+package com.cwy.exam.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.cwy.exam.demo.interceptor.BeforeActionInterceptor;
+
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
+	// BeforeActionInterceptor 불러오기
+	@Autowired
+	BeforeActionInterceptor beforeActionInterceptor;
+
+	// /resource/common.css
+	// 인터셉터 적용
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**")
+				.excludePathPatterns("/error");
+	}
+
+}
