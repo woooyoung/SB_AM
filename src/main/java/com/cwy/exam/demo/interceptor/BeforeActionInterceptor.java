ba75037d1b631a5c233f9863bca6bea567d7a793
woooyoung <axdsw121@gmail.com>
2022-10-11 17:46:21 +0900

    BeforeActionInterceptor 구현

---
+++ src/main/java/com/cwy/exam/demo/interceptor/BeforeActionInterceptor.java
@@ -0,0 +1,18 @@
+package com.cwy.exam.demo.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class BeforeActionInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
+		System.out.println("실행됨?");
+		return HandlerInterceptor.super.preHandle(req, resp, handler);
+	}
+
+}
