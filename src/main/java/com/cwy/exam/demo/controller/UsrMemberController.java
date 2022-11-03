a6bab7248da24f28eb3c35c86e4b66a66c7b1caf
woooyoung <axdsw121@gmail.com>
2022-11-03 17:24:59 +0900

    마이페이지

--- src/main/java/com/cwy/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/cwy/exam/demo/controller/UsrMemberController.java
@@ -106,4 +106,10 @@ public class UsrMemberController {
 		return Ut.jsReplace("로그아웃 되었습니다", "/");
 	}
 
+	@RequestMapping("usr/member/myPage")
+	public String showMyPage() {
+
+		return "usr/member/myPage";
+	}
+
 }
